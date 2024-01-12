f07408900649e8f48cda87611a777c336ff4d9ab
rb.balestrin <therealrenanbez@gmail.com>
2024-01-11 23:29:52 -0300

    Add GLTFLoader and animate ring

--- main.js
+++ main.js
@@ -9,16 +9,56 @@ import * as dat from "dat.gui";
 import Lenis from "@studio-freight/lenis";
 
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
+const loader = new GLTFLoader();
+let ring = null;
+let contactRotation = false;
+let renderer, scene, camera;
 
 function initThreejs() {
   const gui = new dat.GUI();
   dat.GUI.toggleHide();
+
   const canvas = document.querySelector("canvas.webgl");
   scene = new THREE.Scene();
+
   const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
   };
+
+  //GLTF Loader
+  loader.load("ring.glb", function (gltf) {
+    ring = gltf.scene;
+    ring.position.set(0, 0, 0);
+    ring.scale.set(0.5, 0.5, 0.5);
+    scene.add(ring);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "section.details",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+      defaults: {
+        ease: "power3.Out",
+        duration: 3,
+      },
+    });
+
+    tl.to(ring.position, {
+      z: 2.5,
+      y: -0.34,
+    });
+    tl.to(
+      ring.rotation,
+      {
+        z: 1,
+      },
+      "<"
+    );
+  });
+
   window.addEventListener("resize", () => {
     // Update sizes
     sizes.width = window.innerWidth;
@@ -33,7 +73,12 @@ function initThreejs() {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     //Camera
-    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+    camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      100
+    );
     camera.position.z = 2;
     camera.position.y = 0;
     camera.position.x = 0;
@@ -46,6 +91,7 @@ function initThreejs() {
     });
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
